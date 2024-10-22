2718cace9d62b38b62c7090421b1f6a9918a7d34
Максим Безручко <bezruchko-m@va.local>
2024-10-22 14:06:10 +0300

    init

---
+++ manifest.ts
@@ -0,0 +1,23 @@
+import { Manifest } from "deno-slack-sdk/mod.ts";
+import GitHubProvider from "./external_auth/github_provider.ts";
+import CreateNewIssueWorkflow from "./workflows/create_new_issue.ts";
+
+/**
+ * Манифест приложения содержит конфигурацию приложения. Этот файл определяет
+ * такие атрибуты, как имя приложения, описание, доступные Workflows   и многое другое.
+ * Узнайте больше: https://api.slack.com/automation/manifest
+ */
+export default Manifest({
+  name: "slack-stasic",
+  description: "Приносит часто используемую функциональность GitHub в Slack",
+  icon: "assets/default_new_app_icon.jpg",
+  externalAuthProviders: [GitHubProvider],
+  workflows: [CreateNewIssueWorkflow],
+  /**
+   * Доменные имена, используемые в удалённых HTTP-запросах, должны быть указаны как исходящие домены.
+   * Если ваша организация использует отдельный домен GitHub Enterprise, добавьте его сюда,
+   * чтобы выполнять API-вызовы к нему из пользовательской функции.
+   */
+  outgoingDomains: ["api.github.com"],
+  botScopes: ["commands", "chat:write", "chat:write.public"],
+});
